d7868e08b322db2aaeb283603cac63ab06076ec6
AKASH TOMAR <akashtomer0786@gmail.com>
2019-06-26 13:37:45 +0530

    Update and rename fib_matrix.cpp to fib_matrix_exp.cpp

---
+++ maths/fib_matrix_exp.cpp
@@ -0,0 +1,30 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int multiply(int F[2][2],int M[2][2]){
+    int x = F[0][0]*M[0][0]+F[0][1]*M[1][0];
+    int y = F[0][0]*M[0][1]+F[0][1]*M[1][1];
+    int z = F[1][0]*M[0][0]+F[1][1]*M[1][0];
+    int w = F[1][0]*M[0][1]+F[1][1]*M[1][1];
+    F[0][0]=x;
+    F[0][1]=y;
+    F[1][0]=z;
+    F[1][1]=w;
+}
+power(int F[][2],int n){
+    int M[2][2]={{1,1},{1,0}};
+    for(int i=1;i<=n-1;i++){
+        multiply(F,M);
+    }
+}
+int fib(int n){
+    int F[2][2] = {{1,1},{1,0}};
+
+    power(F,n-1);
+    return F[0][0];
+}
+
+
+int main(){
+    cout<<fib(5);
+}
