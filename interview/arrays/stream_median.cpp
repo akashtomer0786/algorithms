01dd74bf47c4857c715ed8ca40de9fb4ecb941a7
AKASH TOMAR <akashtomer0786@gmail.com>
2019-06-28 21:31:52 +0530

    Create stream_median.cpp
    leetcode

---
+++ interview/arrays/stream_median.cpp
@@ -0,0 +1,35 @@
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+        ///max queue
+    priority_queue<int> small;
+    ///min  queue
+    priority_queue<int,vector<int>,greater<int>> large;
+
+    
+    void addNum(int num) {
+                if(small.empty()||small.top()>num){
+            small.push(num);
+        }
+        else{
+            large.push(num);
+        }
+        if(small.size()>large.size()+1){
+            large.push(small.top());
+            small.pop();
+        }
+        if(large.size()>small.size()+1){
+            small.push(large.top());
+            large.pop();
+        }
+    }
+    
+    double findMedian() {
+            if(small.size()==large.size()){
+             return 1.0*(small.top()+large.top())/2;
+            }
+         else{
+             return small.size()>large.size()?small.top():large.top();
+         }
+    }
+};
