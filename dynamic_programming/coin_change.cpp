50e767a682f1325da78d49ffcf5c7f1c43b08f0b
AKASH TOMAR <akashtomer0786@gmail.com>
2020-02-27 19:12:30 +0530

    Create coin_change.cpp

---
+++ dynamic_programming/coin_change.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int ans=0;
+vector<int>a;
+
+void coin_change(int m){
+    int n=a.size();
+    int dp[m+1];
+    
+    dp[0]=1;
+    for(int i=0;i<=n-1;i++){
+        for(int j=1;j<=m;j++){
+            int x=0,y=0;
+            if(j-a[i]>=0)x=dp[j-a[i]];
+            if(i-1>=0)y=dp[j];
+            dp[j]=x+y;
+        }
+    }
+    ans = dp[m];
+}
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--){
+	    ans=0;
+	    int n;
+	    cin>>n;
+	    a.resize(n);
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    int m;
+	    cin>>m;
+	    //sort(a.begin(),a.end());
+	    coin_change(m);
+	    cout<<ans<<endl;
+	    
+	}
+	return 0;
+}
