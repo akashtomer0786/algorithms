16e501b21511eab326933a43a97a9a47a2259d4d
AKASH TOMAR <akashtomer0786@gmail.com>
2020-03-25 15:27:50 +0530

    Update Count ways to N'th Stair(Order does not matter).cpp

--- dynamic_programming/Count ways to N'th Stair(Order does not matter).cpp
+++ dynamic_programming/Count ways to N'th Stair(Order does not matter).cpp
@@ -1,4 +1,5 @@
 //https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter/0/
+//more robust n/2+1
 #include<iostream>
 using namespace std;
 int main()
@@ -11,7 +12,7 @@ int main()
 	    int dp[n+1];
 	    dp[0]=1;
 	    for(int i=1;i<=n;i++){
-	        if(i-1>=0)dp[i]=1;
+	        if(i-1>=0)dp[i]=1; //with all ones
 	        if(i-2>=0)dp[i]+=dp[i-2];
 	    }
 	    cout<<dp[n]<<endl;
