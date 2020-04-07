13a5763bdcc8a524c7517c1e23ccaff805c1031c
AKASH TOMAR <akashtomer0786@gmail.com>
2020-04-07 18:15:18 +0530

    Update min_sum_part.cpp

--- dynamic_programming/min_sum_part.cpp
+++ dynamic_programming/min_sum_part.cpp
@@ -1,3 +1,5 @@
+///minimum sum partition 
+//https://www.geeksforgeeks.org/partition-a-set-into-two-subsets-such-that-the-difference-of-subset-sums-is-minimum/
 #include<bits/stdc++.h>
 using namespace std;
 
