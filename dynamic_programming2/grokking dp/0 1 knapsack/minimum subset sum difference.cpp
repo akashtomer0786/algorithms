b3b67cbbc9c4f991aac3c6bbb2cb00416c346c1a
AKASH TOMAR <akashtomer0786@gmail.com>
2020-05-19 07:46:37 +0530

    Create minimum subset sum difference.cpp

---
+++ dynamic_programming2/grokking dp/0 1 knapsack/minimum subset sum difference.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findmin(int arr[],int n){
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+
+    bool dp[n+1][sum+1];
+
+    for(int i=0;i<=n;i++)
+        dp[i][0]=true;
+
+    for(int i=1;i<=sum;i++)
+        dp[0][i]=false;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=sum;j++){
+
+
+            if(arr[i-1]<=j)
+                dp[i][j] = dp[i-1][j]|dp[i-1][j-arr[i-1]];
+            else dp[i][j] = dp[i-1][j];    
+        }
+    }
+
+    int diff = INT_MAX;
+    ///Sum of set1 is j and therefore sum of set2 is (totalSum-j) and therefore difference of sets is (totalSum-j) - (j) i.e. totalSum-2*j.
+    for(int j=sum/2;j>=1;j--){
+        if(dp[n][j]==true){
+            diff=sum-2*j;
+            break;
+        }
+    }
+    return diff;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)cin>>a[i];
+        int ans = findmin(a,n);
+        cout<<ans<<endl;
+    }
+}
