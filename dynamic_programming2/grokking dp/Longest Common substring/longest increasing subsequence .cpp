e772084e9d36c7c33360bb96ae8730d88361520f
AKASH TOMAR <akashtomer0786@gmail.com>
2020-05-20 22:39:26 +0530

    Create longest increasing subsequence .cpp

---
+++ dynamic_programming2/grokking dp/Longest Common substring/longest increasing subsequence .cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0)return 0;
+        int dp[n+1];
+        dp[0]=1;
+        int ans=1;
+        for(int i=1;i<n;i++){
+            int premax=0;
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]&&dp[j]>premax){
+                    premax=dp[j];
+                }
+            }
+            dp[i]=1+premax;
+            ans=max(ans,dp[i]);
+        }
+        return dp[n-1];
+    }
+};
+
+class Solution {
+public:
+    int bs(int temp[],int i,int j,int x){
+        while(i<j){
+            int mid = (i+j)>>1;
+            if(temp[mid]<x)i=mid+1;
+            else j=mid;
+        }
+        return i;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int sz=0;
+        //suppose we have the sequence 1 3 4 8 and we want to put 5 here then we will search
+        //the next greater element put 5 there so that when any element like 6 comes we can extend
+        //our length but this was not possible if 8 was not replaced 
+        int temp[nums.size()];
+        for(int x : nums){
+            int mid = bs(temp,0,sz,x);
+            temp[mid]=x;
+            if(mid==sz)sz++;
+        }
+        return sz;
+    }
+};
