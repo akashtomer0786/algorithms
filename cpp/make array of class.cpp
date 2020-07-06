2c630de05cb1cfd3a3d5f5775a5900a9920bda9e
AKASH TOMAR <akashtomer0786@gmail.com>
2020-07-06 12:10:54 +0530

    Create make array of class.cpp

---
+++ cpp/make array of class.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class name{
+public:
+    string first;
+    string last;
+    name(){
+        first="";
+        last="";
+    }
+    name(string a,string b){
+        first=a;
+        last=b;
+    }
+};
+int main(){
+    name arr[3]; ///to make this array we need default constructor param
+    ///paramatrized constructor is not really helpful here
+}
