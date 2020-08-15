78e72f7c82560e0186be58724e7ca615a0b319de
AKASH TOMAR <akashtomer0786@gmail.com>
2020-08-15 18:32:18 +0530

    Create printf2.c

---
+++ c_interview/printf2.c
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    printf("%s\n","Hello");
+    
+    printf("%10s","Hello");
+    ///it need to print 10 characters if there are not enough 10 characters then
+    /// it will print some white extra white spaces here output will be "     Hello"
+}
