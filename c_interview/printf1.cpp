e1a73dcff07d4623061bdc1047af14666b8b0fc1
AKASH TOMAR <akashtomer0786@gmail.com>
2020-08-15 18:29:46 +0530

    Create printf1.cpp

---
+++ c_interview/printf1.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    printf("%d",printf("%s","Hello world!"));
+    ///will return the number of characters that it successfully printed on the screen.
+    return 0;
+}
