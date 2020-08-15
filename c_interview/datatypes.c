94528a57537737581b8725a425458cf79dc422f8
AKASH TOMAR <akashtomer0786@gmail.com>
2020-08-15 18:48:38 +0530

    Create datatypes.c

---
+++ c_interview/datatypes.c
@@ -0,0 +1,17 @@
+signed int i;
+signed i; unsigned i; long i; long long i;  ///compiler will assume int in these cases
+long long int i;
+so all are correct.
+
+char   1 byte   0 to 255
+unsigned char 1 byte 0 to 255
+signed char   1 byte -128 to 127
+
+int 4 bytes
+unsigned int 4 bytes
+short 2 bytes
+unsigned short 2 bytes
+
+float 4 byte
+double 8 byte
+long double 10 byte
