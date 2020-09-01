26c90e204e790ef055c1a8e072b4c51968bad58c
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-01 16:32:22 +0530

    Create core_dump.cpp

---
+++ cpp/important_int/core_dump.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+/// first compile it like g++ -g app.cpp
+///it will create core file
+/// gdb a.out core
+///it will give you the line number where segmentation fault occured
+int main(){
+    int *i = NULL;
+    *i = 10;
+    cout<<*i<<endl;
+}
