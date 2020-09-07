cc94158ce903520e41e1c45d10d8025f90cfd450
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-07 20:54:23 +0530

    Update enum1.cpp

--- cpp/curcpp/enum1.cpp
+++ cpp/curcpp/enum1.cpp
@@ -3,6 +3,9 @@ using namespace std;
 enum State {working=1,failed=0};
 enum week  {Mon , Tue , Wed , Thur , Fri , Sat , Sund};
 
+Enumeration is a user defined datatype in C language.
+It is used to assign names to the integral constants which makes a program easy to read and maintain. The keyword “enum” is used to declare an enumeration.
+
 int main(){
     State mymachinestate = failed;
     if(mymachinestate){
