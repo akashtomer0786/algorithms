088771036a87b2e7cd8b121e50fa73f507337ab7
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-11 09:39:03 +0530

    Create struct_cpp.cpp

---
+++ cpp/curcpp/struct_cpp.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+///both will compile
+struct queuez{
+    int x;
+
+    void enqueue(struct queuez s){
+
+    }
+};
+struct queuef{
+    int x;
+    void enqueue(struct queuef* s){
+
+    }
+};
+int main(){
+
+}
