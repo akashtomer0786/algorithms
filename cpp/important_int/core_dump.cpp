abf81179a9d86c272e6cc84ee6e26f1a0ee3eb00
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-14 21:27:13 +0530

    Update core_dump.cpp

--- cpp/important_int/core_dump.cpp
+++ cpp/important_int/core_dump.cpp
@@ -7,6 +7,7 @@ using namespace std;
 /// gdb a.out core
 ///it will give you the line number where segmentation fault occured
 ///we are talking about here linux
+//A “core dump” is a snapshot of memory at the instant the program crashed, typically saved in a file called “core”
 int main(){
     int *i = NULL;
     *i = 10;
