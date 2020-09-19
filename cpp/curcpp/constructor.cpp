345a80976a19fbf461f72b9cf9fc5434446402a9
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-19 20:34:56 +0530

    Update constructor.cpp

--- cpp/curcpp/constructor.cpp
+++ cpp/curcpp/constructor.cpp
@@ -1,2 +1,4 @@
 In C++, the constructor cannot be virtual, because when a constructor of a class is executed there is no virtual table in the memory, 
 means no virtual pointer defined yet. So, the constructor should always be non-virtual.
+  
+cpp doesn't have static constructor
