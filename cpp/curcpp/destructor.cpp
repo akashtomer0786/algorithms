3efbac7976a4404f8dcbb86d59e20d9e41e9a713
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-19 20:23:24 +0530

    Create destructor.cpp

---
+++ cpp/curcpp/destructor.cpp
@@ -0,0 +1,3 @@
+A destructor takes no arguments and has no return type. 
+Its address cannot be taken. Destructors cannot be declared const, volatile, const volatile or static. 
+A destructor can be declared virtual or pure virtual.
