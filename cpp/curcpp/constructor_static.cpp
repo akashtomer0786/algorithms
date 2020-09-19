ce337476fe52ce3d86a0a472cca470fb19aa359b
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-19 22:03:30 +0530

    Create constructor_static.cpp

---
+++ cpp/curcpp/constructor_static.cpp
@@ -0,0 +1,5 @@
+The purpose of a constructor is to initialize the contents of an instance of the class.
+
+Static methods don't have an instance associated with them.
+
+Hence there is no such thing as a static constructor.
