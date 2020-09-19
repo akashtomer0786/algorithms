3d4ade6cc3b6555543be62bca1cd9088e6e2e998
AKASH TOMAR <akashtomer0786@gmail.com>
2020-09-19 20:27:44 +0530

    Update destructor.cpp

--- cpp/curcpp/destructor.cpp
+++ cpp/curcpp/destructor.cpp
@@ -1,3 +1,7 @@
 A destructor takes no arguments and has no return type. 
 Its address cannot be taken. Destructors cannot be declared const, volatile, const volatile or static. 
 A destructor can be declared virtual or pure virtual.
+  
+  
+  The keyword static means that the variable is independent of instances. That's why you can access static variables 
+  and methods without instantiating an object from the class in the first place. That's why destroying an instance will not affect any static variables.
