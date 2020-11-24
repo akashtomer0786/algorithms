77f79d43d97c055c2714af6f6b8e2cf952029ea4
AKASH TOMAR <akashtomer0786@gmail.com>
2020-11-24 10:11:23 +0530

    Update ptr1.cpp

--- cpp/curcpp/ptr1.cpp
+++ cpp/curcpp/ptr1.cpp
@@ -7,6 +7,9 @@ int main(){
     --(*ptr);  ///this will change the value inside ptr;
     (*ptr)--;  ///this will change the value inside ptr
     *ptr++; ///this will perform arithmatic operation on ptr
+    
+    /// from stackoverflow  *ptr++ - going from left-to-right, dereference the pointer, and then increment the pointer value (not what it points to, due to the precedence of postfix over dereference)
+    ///above there is error
     cout<<ptr<<endl;
     *ptr++;
     cout<<ptr-1<<endl;
