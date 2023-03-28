65517480fd1f538e7763ea0cfb47f945b81e7ba2
AKASH TOMAR <akashtomer0786@gmail.com>
2023-03-28 15:22:01 +0530

    Create design_decorator_pattern.cpp

---
+++ cpp/curcpp/design_decorator_pattern.cpp
@@ -0,0 +1,58 @@
+
+#include<iostream>
+using namespace std;
+
+class Burger{
+public:
+    virtual int get_cost() = 0;
+};
+
+class ConcretteBurger: public Burger{
+public:
+    int get_cost(){
+        return 3;
+    }
+};
+
+class BurgerDecorator:public Burger{
+private:
+    Burger *b;
+
+public:
+    BurgerDecorator(Burger* bb){
+      b = bb;
+    }
+    ~BurgerDecorator(){
+        delete b;
+    }
+    int get_cost(){
+        return b->get_cost();
+    }
+};
+
+class Tomato:public BurgerDecorator{
+public:
+    Tomato(Burger *b):BurgerDecorator(b){
+
+    }
+    int get_cost(){
+
+        return BurgerDecorator::get_cost() + 4;
+    }
+};
+class Cheese:public BurgerDecorator{
+public:
+    Cheese(Burger *b):BurgerDecorator(b){}
+    int get_cost(){
+        return BurgerDecorator::get_cost()+3;
+
+    }
+};
+
+int main(){
+    Burger* burger = new ConcretteBurger();
+    Burger* burgerWithCheese = new Cheese(burger);
+    cout<<burgerWithCheese->get_cost();
+
+    system("pause");
+}
