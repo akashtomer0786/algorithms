eafa9c9caa3055679f621c5c5b8170c09457e2cd
AKASH TOMAR <akashtomer0786@gmail.com>
2023-04-12 15:13:13 +0530

    Create association.cpp

---
+++ cpp/curcpp/association.cpp
@@ -0,0 +1,4 @@
+The composition is stronger than Aggregation.  In Short, a relationship between two objects is referred to as an association,
+and an association is known as composition when one object owns another while an association is known as aggregation when one object uses another object.
+
+https://javarevisited.blogspot.com/2014/02/ifference-between-association-vs-composition-vs-aggregation.html#axzz7yetuLBdv
